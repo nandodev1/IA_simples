0b6bcb22d9da595c28d9b3e2604640eece51b469
Fernando santos <fernando.santosgomes@ufrpe.br>
2024-02-06 20:03:39 -0300

    Conclusão da classe camada
    Classe que cria uma camada com saída relu tanto escondida quanto de saída.

---
+++ include/Camada.h
@@ -0,0 +1,76 @@
+#ifndef CAMADA_H
+#define CAMADA_H
+
+#include<iostream>
+#include<vector>
+
+#include "Perceptron.h"
+
+using namespace std;
+
+
+class Camada
+{
+    private:
+        vector<Perceptron> neuronios;
+        //Guarda os ultimos valores da ultima computação
+        vector<float> entradas;
+        vector<float> saidas;
+    
+    public:
+
+        Camada( int quant_entradas, int quant_saidas);
+        vector<float> saida(vector<float> entradas);
+        vector<Perceptron> get_neuronios();
+        vector<float> get_pesos();
+        ~Camada();
+};
+
+vector<float> Camada::get_pesos()
+{
+    vector<float> pesos;
+    Perceptron neuro;
+    int quant_neuronio = this->neuronios.size();
+    //Pega o primeiro neuronio para definir a quantidade pesos
+    //considerando que todos neuronios das camadas tem a mesma quantidade de neuronio
+    int quant_pesos = this->neuronios[0].pesos.size();
+    for ( int i = 0; i < quant_neuronio; i++)
+        for( int j = 0; j < quant_pesos; j++)
+        {
+            neuro = this->neuronios[i];
+            pesos.push_back( neuro.pesos[j]);
+        }
+
+    return pesos;
+}
+
+vector<Perceptron> Camada::get_neuronios()
+{
+    return this->neuronios;
+}
+
+Camada::Camada( int quant_entradas, int quant_saidas)
+{
+    for ( int i = 0; i < quant_entradas;i++)
+        this->entradas.push_back(0);
+        this->saidas.push_back(0);
+
+    for ( int i = 0; i < quant_saidas; i++)
+        this->neuronios.push_back( Perceptron( quant_entradas, SAIDA_RELU));
+};
+
+
+vector<float> Camada::saida(vector<float> entradas)
+{
+    for( int i = 0; i < this->neuronios.size(); i++)
+    {
+        this->saidas[i] = this->neuronios[i].saida( entradas);
+    }
+    return this->saidas;
+};
+
+Camada::~Camada()
+{
+}
+
+#endif
--- main.cpp
+++ main.cpp
@@ -1,26 +1,30 @@
 #include <iostream>
-#include "src/Perceptron.cpp"
+#include "include/Perceptron.h"
+#include "include/Camada.h"
 
 using namespace std;
 
 int main()
 {
-    Perceptron * neuro = new Perceptron( 2, SAIDA_RELU);
     vector<float> entr_treino = { 0, 0};
+
+    Camada camada = Camada( 2, 10);
+
     for (int i = 0; i < 10; i ++)
     {
         entr_treino[0] = -1;
         entr_treino[1] = -1;
-        neuro->treinamento( { -1, -1}, -1, 0.5);
+        camada.get_neuronios()[0].treinamento( { -1, -1}, -1, 0.5);
         entr_treino[0] = -1;
         entr_treino[1] = 1;
-        neuro->treinamento( entr_treino, 1, 0.5);
+        camada.get_neuronios()[0].treinamento( entr_treino, 1, 0.5);
         entr_treino[0] = 1;
         entr_treino[1] = -1;
-        neuro->treinamento( entr_treino, 1, 0.5);
+        camada.get_neuronios()[0].treinamento( entr_treino, 1, 0.5);
     }
-    entr_treino[0] = -1;
+    entr_treino[0] = 1;
     entr_treino[1] = -1;
-    cout <<  neuro->saida( entr_treino) << endl;
+    cout <<  camada.saida( entr_treino)[0] << endl;
+    camada.get_pesos();
     return 0;
 }
