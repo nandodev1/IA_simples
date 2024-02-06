96fef09fe8fea27422355f530521f33545ea7bb4
Fernando santos <fernando.santosgomes@ufrpe.br>
2024-02-06 17:55:19 -0300

    substituição float * por vector<float>

--- main.cpp
+++ main.cpp
@@ -6,12 +6,12 @@ using namespace std;
 int main()
 {
     Perceptron * neuro = new Perceptron( 2, SAIDA_RELU);
-    float entr_treino [2] = { -1, -1};
+    vector<float> entr_treino = { 0, 0};
     for (int i = 0; i < 10; i ++)
     {
         entr_treino[0] = -1;
         entr_treino[1] = -1;
-        neuro->treinamento( entr_treino, -1, 0.5);
+        neuro->treinamento( { -1, -1}, -1, 0.5);
         entr_treino[0] = -1;
         entr_treino[1] = 1;
         neuro->treinamento( entr_treino, 1, 0.5);
