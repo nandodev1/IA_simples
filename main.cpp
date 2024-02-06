82c2cfc4a39962fdea3929c1aefb0a6d4b8504d5
Fernando santos <fernando.santosgomes@ufrpe.br>
2024-02-06 20:13:22 -0300

    troca

--- main.cpp
+++ main.cpp
@@ -8,23 +8,23 @@ int main()
 {
     vector<float> entr_treino = { 0, 0};
 
-    Camada camada = Camada( 2, 10);
+    Camada * camada = new Camada( 2, 10);
 
     for (int i = 0; i < 10; i ++)
     {
         entr_treino[0] = -1;
         entr_treino[1] = -1;
-        camada.get_neuronios()[0].treinamento( { -1, -1}, -1, 0.5);
+        camada->get_neuronios()[0].treinamento( { -1, -1}, -1, 0.5);
         entr_treino[0] = -1;
         entr_treino[1] = 1;
-        camada.get_neuronios()[0].treinamento( entr_treino, 1, 0.5);
+        camada->get_neuronios()[0].treinamento( entr_treino, 1, 0.5);
         entr_treino[0] = 1;
         entr_treino[1] = -1;
-        camada.get_neuronios()[0].treinamento( entr_treino, 1, 0.5);
+        camada->get_neuronios()[0].treinamento( entr_treino, 1, 0.5);
     }
-    entr_treino[0] = 1;
+    entr_treino[0] = -1;
     entr_treino[1] = -1;
-    cout <<  camada.saida( entr_treino)[0] << endl;
-    camada.get_pesos();
+    cout <<  camada->saida( entr_treino)[0] << endl;
+    //camada.get_pesos();
     return 0;
 }
