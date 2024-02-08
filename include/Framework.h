ce31f5f212d09cc6dc9834f7cf3aa73e6e20a033
nando <nandobink6@gmail.com>
2024-02-08 18:50:25 -0300

    Migração do repositorio local para o github e afição da classe Framework

--- include/Framework.h
+++ include/Framework.h
@@ -1,24 +1,40 @@
 
 #include<GL/glut.h>
+#include<string>
 
+//Referancia para criação da classe
+// https://www.inf.pucrs.br/~manssour/OpenGL/PrimeiroPrograma.html
 class Framework
 {
-private:
-    int largura;
-    int altura;
-public:
-    Framework(int largura, int altura);
-    ~Framework();
-    void Desenha();
+    private:
+        int largura;
+        int altura;
+    public:
+        Framework(string titulo, int largura, int altura);
+        ~Framework();
+        void Desenha();
+        void inicializa();
 };
 
-Framework::Framework( int largura, int altura)
+Framework::Framework(string titulo, int largura, int altura)
 {
-
+	glutInitDisplayMode(GLUT_SINGLE | GLUT_RGB);
+	glutCreateWindow("IA_simple");
+	glutDisplayFunc(this->Desenha);
+	this->inicializa();
+	glutMainLoop();
 }
 
 Framework::~Framework()
 {
+}
+
+
+// Inicializa parâmetros de rendering
+void Inicializa (void)
+{   
+    // Define a cor de fundo da janela de visualização como preta
+    glClearColor(0.0f, 0.0f, 0.0f, 1.0f);
 }
 
 // Função callback chamada para fazer o desenho
--- main.cpp
+++ main.cpp
@@ -2,9 +2,26 @@
 #include "include/Perceptron.h"
 #include "include/Camada.h"
 
+
+void testes_redes();
+void testes_framework();
+
 using namespace std;
 
 int main()
+{   
+    testes_framework();
+    return 0;
+}
+
+
+void testes_framework()
+{
+//    Framework * fw = new Framework( "Ai_simple", 500, 500);
+    cout << "Tudo ok" << endl;
+}
+
+void testes_redes()
 {
     Camada * camada_1 = new Camada( 2, 2);
     Camada * camada_saida = new Camada( 2, 1);
@@ -12,12 +29,11 @@ int main()
     camada_1->set_pesos({ -0.5, 1, 1, -1.5, 1, 1});
     camada_saida->set_pesos({-0.5, 1, -2});
 
-    vector<float> input = { 0, 1};
+    vector<float> input = { 1, 1};
     vector<float> saida_camada_1;
 
     saida_camada_1 = camada_1->saida( input);
     
     float saida_camada_saida = camada_saida->saida(saida_camada_1)[0];
     cout << saida_camada_saida << endl;
-    return 0;
 }
