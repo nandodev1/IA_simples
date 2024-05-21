cf65b5a39d1e08d96b08a445c5a0d7af3d69eb9b
Fernando santos gomes <nandobink6@gmail.com>
2024-05-21 19:34:21 -0300

    organização do arquivo main_teste

--- source/init.cpp
+++ source/init.cpp
@@ -1,14 +1,17 @@
+#include "IA/Agente.h"
+
 void setup()
 {
 
 }
 
 float y = 0;
+Agente * ag1 = new Agente();
 
 void loop()
 {
     y += 0.1;
-    retangulo( 300, y, 15.0, 15.0, { 100, 250, 50});
+    
     retangulo( 100, y+ 50, 15.0, 15.0, { 100, 50, 50});
     retangulo( y, y+ 50, 15.0, 15.0, { 100, 150, 50});
 	
